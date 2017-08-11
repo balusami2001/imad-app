19547bfd4f1b9cf7847cbd05be450f2a834fd1d8
balusami2001 <30752730+balusami2001@users.noreply.github.com>
2017-08-10 22:06:29 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,8 @@
-console.log('Loaded!');
-var element =  document.getElementById('main-text');
-element.innerHTML='i am bala.';
-
-
-var img=document.getElementById('madi');
-var marginRight=0;
-function moveLeft (){
-    marginRight=marginRight + 1;
-    img.style.marginRight = marginRight + 'px';
+var button = document.getElementById("counter");
+var counter = 0;
+button.onclick = function(){
     
-}
-img.onclick = function(){
-    var interval=setInterval(moveLeft,50);
-    
+counter = counter + 1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
 };
