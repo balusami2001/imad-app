f9b2001098c2a0014cd77e3c59b57f51d7089691
balusami2001 <30752730+balusami2001@users.noreply.github.com>
2017-08-10 22:22:01 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,21 @@
 var button = document.getElementById("counter");
-var counter = 0;
+
+
 button.onclick = function(){
     
-counter = counter + 1;
+    var request = new XMLHttpRequest();
+Request.onreadystatechange = function(){
+  
+  if (Request.readyState === XMLHttpRequest.DONE){
+  
+  if(Request.status===200){
+  counter = counter + 1;
 var span = document.getElementById('count');
 span.innerHTML = counter.toString();
+  
+     }
+  }
+  Request.open('get', 'http://balusami2001.imad.hasura-app.io/counter',true);
+  Request.send(null);
+};
 };
